b0b36c596173276328a3ee48c495a2bd4cb72bcb
kamolesh <kamoleshbachar@gmail.com>
2024-01-28 00:17:33 -0500

    create profile component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
 //import Student from './Student';
 import Footer from './components/Footer';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img 
-          src='./p/k.jpg' 
-          style={{
-            width: 200,
-            height: 200,
-            borderRadius: 100
-          }} 
-          alt='Kamolesh Bachar'
-        />
-
-        <h1>Kamolesh Bachar</h1>
+      <Profile/>
       </header>
+      
       {/* <Student></Student> */}
       <Footer/>
     </div>
---
+++ src/components/Profile.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Profile(){
+
+    return(
+        <div>
+            <img 
+            src='./p/k.jpg' 
+            style={{
+                width: 200,
+                height: 200,
+                borderRadius: 100
+            }} 
+            alt='Kamolesh Bachar'
+            />
+
+            <h1>Kamolesh Bachar</h1>
+        </div>
+    )
+}
+
+export default Profile;
